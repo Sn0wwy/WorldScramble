ba951d8990d82a7427c2d43afadd0844bf66556c
Ignacio Alavés <nachoalasanchez@gmail.com>
2023-07-21 17:53:15 +0200

    Initial Commit

---
+++ WorldScramble/WorldScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldScrambleApp.swift
+//  WorldScramble
+//
+//  Created by Nacho Alaves on 21/7/23.
+//
+
+import SwiftUI
+
+@main
+struct WorldScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
